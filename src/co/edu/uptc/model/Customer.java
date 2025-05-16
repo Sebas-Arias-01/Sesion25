c8165babe5af415194ce3703a9a1a4b135f782bd
Sebas-Arias-01 <sebastian.arias02@uptc.edu.co>
2025-05-16 10:46:56 -0500

    customer ?

--- src/co/edu/uptc/model/Customer.java
+++ src/co/edu/uptc/model/Customer.java
@@ -1,57 +1,6 @@
 package co.edu.uptc.model;
 
 public class Customer {
-    private int id;
-    private String address;
-    private String phone;
-    private String email;
 
-    public Customer() {
-    
-    }
-
-    public Customer(int id, String address, String phone, String email) {
-        this.id = id;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer [id=" + id + ", address=" + address + ", phone=" + phone + ", email=" + email + "]";
-    }
 
 }
--- src/co/edu/uptc/model/Shipping.java
+++ src/co/edu/uptc/model/Shipping.java
@@ -2,4 +2,44 @@ package co.edu.uptc.model;
 
 public class Shipping {
 
+    private int id;
+    private String nameTransport;
+    private String status;
+    private String date;
+    
+    public Shipping() {
+    }
+    
+    public Shipping(int id, String nameTransport, String status, String date) {
+        this.id = id;
+        this.nameTransport = nameTransport;
+        this.status = status;
+        this.date = date;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNameTransport() {
+        return nameTransport;
+    }
+    public void setNameTransport(String nameTransport) {
+        this.nameTransport = nameTransport;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
 }
