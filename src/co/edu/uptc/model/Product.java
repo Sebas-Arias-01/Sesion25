21cb617a1d8d68c728e4bbcb8629bc5357beab9b
Sebas-Arias-01 <sebastian.arias02@uptc.edu.co>
2025-05-16 10:45:42 -0500

    Clase producto y shipping

--- src/co/edu/uptc/model/Product.java
+++ src/co/edu/uptc/model/Product.java
@@ -2,4 +2,36 @@ package co.edu.uptc.model;
 
 public class Product {
 
+    private int id;
+    private String description;
+    private String presentation;
+    
+    public Product() {
+        
+    }
+    
+    public Product(int id, String description, String presentation) {
+        this.id = id;
+        this.description = description;
+        this.presentation = presentation;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getPresentation() {
+        return presentation;
+    }
+    public void setPresentation(String presentation) {
+        this.presentation = presentation;
+    }
+
 }
